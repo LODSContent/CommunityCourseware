157040e591ca193072c3ffb2b532d8ad31cd363c
MGrasdal <martin_grasdal@hotmail.com>
2022-06-13 21:20:09 -0600

    Added files

---
+++ scc742/module-08/Scripts/CreateCAUsers.ps1
@@ -0,0 +1,30 @@
+﻿# Create OUs.
+
+New-ADOrganizationalUnit -Name Corp -Path 'DC=Hexelo,DC=COM'
+New-ADOrganizationalUnit -Name Users -Path 'OU=Corp,DC=Hexelo,DC=COM'
+New-ADOrganizationalUnit -Name Computers -Path 'OU=Corp,DC=Hexelo,DC=COM'
+New-ADOrganizationalUnit -Name AdminGroups -Path 'OU=Corp,DC=Hexelo,DC=COM'
+New-ADOrganizationalUnit -Name PKI -Path 'OU=AdminGroups,OU=Corp,DC=Hexelo,DC=COM'
+New-ADOrganizationalUnit -Name PKIServers -Path 'OU=Computers,OU=Corp,DC=Hexelo,DC=COM'
+
+# Create users from csv file
+Import-Csv D:\LabFiles\Scripts\users.csv | foreach {New-AdUser -Name "$($_.DisplayName)" -SAMAccountName ($_.SAM) -UserPrincipalName ($_.SAM + "@Hexelo.com")  -DisplayName "$($_.DisplayName)" -GivenName "$($_.Fname)" -Surname "$($_.Lname)" -AccountPassword (ConvertTo-SecureString -AsPlainText "Passw0rd!" -Force)  -PasswordNeverExpires $true -Path 'OU=Users,OU=Corp,DC=Hexelo,DC=com'}  
+
+# Enable all accounts created in the Users OU
+Get-ADUser -Filter * -SearchBase "OU=Users,OU=Corp,DC=Hexelo,DC=com" | Where-Object{$_.Enabled -like "False"} | Enable-AdAccount
+
+#Create PKI groups and add members
+
+dsadd group "cn=CA-Admins,ou=pki,ou=AdminGroups,ou=corp,dc=hexelo,dc=com" -scope g -desc "Group for Administering CA" -members "cn=CAAdmin1,ou=Users,ou=corp,dc=hexelo,dc=com" "cn=CAAdmin2,ou=Users,ou=corp,dc=hexelo,dc=com"
+
+dsadd group "cn=Cert-Managers,ou=pki,ou=AdminGroups,ou=corp,dc=hexelo,dc=com" -scope g -desc "Group for Certificate Administration" -members "cn=CertManager1,ou=Users,ou=corp,dc=hexelo,dc=com" "cn=CertManager2,ou=Users,ou=corp,dc=hexelo,dc=com"
+
+dsadd group "cn=Cert-Template-Admins,ou=pki,ou=AdminGroups,ou=corp,dc=hexelo,dc=com" -scope g -desc "Group for Certificate Template Administration" -members "cn=CertTemplate1,ou=Users,ou=corp,dc=hexelo,dc=com" "cn=CertTemplate2,ou=Users,ou=corp,dc=hexelo,dc=com"
+
+dsadd group "cn=Cert-Auditors,ou=pki,ou=AdminGroups,ou=corp,dc=hexelo,dc=com" -scope g -desc "Group for CA Auditing" -members "cn=CAAuditor1,ou=Users,ou=corp,dc=hexelo,dc=com" "cn=CAAuditor2,ou=Users,ou=corp,dc=hexelo,dc=com"
+
+dsadd group "cn=Cert-Backup-Operators,ou=pki,ou=AdminGroups,ou=corp,dc=hexelo,dc=com" -scope g -desc "Group for Backing up CA" -members "cn=CABackup1,ou=Users,ou=corp,dc=hexelo,dc=com" "cn=CABackup2,ou=Users,ou=corp,dc=hexelo,dc=com"
+
+dsadd group "cn=Key-Recovery-Agents,ou=pki,ou=AdminGroups,ou=corp,dc=hexelo,dc=com" -scope g -desc "Group for Key Recovery Agents" -members "cn=KRA1,ou=Users,ou=corp,dc=hexelo,dc=com" "cn=KRA2,ou=Users,ou=corp,dc=hexelo,dc=com"
+
+dsadd group "cn=Admins,ou=pki,ou=AdminGroups,ou=corp,dc=hexelo,dc=com" -scope g -desc "Group for all CA and Certificate Admins" -members "cn=CA-Admins,ou=pki,ou=AdminGroups,ou=corp,dc=hexelo,dc=com" "cn=Cert-Managers,ou=pki,ou=AdminGroups,ou=corp,dc=hexelo,dc=com" "cn=Cert-Template-Admins,ou=pki,ou=AdminGroups,ou=corp,dc=hexelo,dc=com" "cn=Cert-Auditors,ou=pki,ou=AdminGroups,ou=corp,dc=hexelo,dc=com" "cn=Cert-Backup-Operators,ou=pki,ou=AdminGroups,ou=corp,dc=hexelo,dc=com"
