fab0f75d49b7260e734ca9598c09cdc8fd01cef6
MGrasdal <martin_grasdal@hotmail.com>
2022-08-06 06:51:19 -0600

    Added files

---
+++ scc030/M365Files/Module4b.ps1
@@ -0,0 +1,16 @@
+Param (
+[Parameter(Mandatory=$true)] [string]$Username,
+[Parameter(Mandatory=$true)] [string]$Password,
+[Parameter(Mandatory=$true)] [string]$ID
+)
+
+$pwd = ConvertTo-SecureString "$Password" -asPlainText -Force
+
+$cred = new-object System.Management.Automation.PSCredential($username,$pwd)
+
+Connect-AzureAD -credential $cred | out-null
+Connect-mgGraph -scopes Files.Read, Files.Read.All, Sites.Read.All -contextscope CurrentUser
+$files =  Try {invoke-mggraphrequest -uri "v1.0/me/drive/root/children" -erroraction ignore} Catch {}
+set-content -path c:\verifyitems\files.txt -value $files.Values.name
+
+write-host "The script has completed. Thank you!"
