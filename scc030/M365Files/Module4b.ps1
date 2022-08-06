22c9291e25a3fa78cdd07b4d8eaecb1fa80f45fd
MGrasdal <martin_grasdal@hotmail.com>
2022-08-06 07:32:41 -0600

    revised script

--- scc030/M365Files/Module4b.ps1
+++ scc030/M365Files/Module4b.ps1
@@ -11,6 +11,8 @@ $cred = new-object System.Management.Automation.PSCredential($username,$pwd)
 Connect-AzureAD -credential $cred | out-null
 Connect-mgGraph -scopes Files.Read, Files.Read.All, Sites.Read.All -contextscope CurrentUser
 $files =  Try {invoke-mggraphrequest -uri "v1.0/me/drive/root/children" -erroraction ignore} Catch {}
-set-content -path c:\verifyitems\files.txt -value $files.Values.name
+$names = $files.Values.name
+$content = $ID,$names
+set-content -path c:\verifyitems\files.txt -value $content
 
 write-host "The script has completed. Thank you!"
